098a04d82c48f4ce8e4047c18c9be39bb19bb9b3
Brian H <61915435+bhaegs@users.noreply.github.com>
2020-10-01 11:22:30 -0500

    Fixed functionality

--- RPS_v2/main.js
+++ RPS_v2/main.js
@@ -1,4 +1,3 @@
-
 // Dark Mode
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
@@ -10,44 +9,25 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'light');
     }    
 }
-
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-// Rock Paper Scissors onclick
+// Global Variables
     const selRock = document.querySelector('#rock');
     const selPaper = document.querySelector('#paper');
     const selScissors = document.querySelector('#scissors');
-
-   document.getElementById('rock').onclick = function() {playRound('rock', computerPlay())};
-   document.getElementById('paper').onclick = function() {playRound('paper', computerPlay())};
-   document.getElementById('scissors').onclick = function() {playRound('scissors', computerPlay())};
-
-
+    
     let = playerScore = 0;
     let = computerScore = 0;
-    let = tie = 0;
 
     let computerChoice = computerPlay();
     let playerChoice = "";
     let playerWins = document.querySelector('#pWins');
-    let drawGame = document.querySelector('#tie')
-    let computerWins = document.querySelector('.cWins');
+    let computerWins = document.querySelector('#cWins');
     let result = document.querySelector('#res');
-    let endGame = document.querySelector('.endGame');
+    let endGame = document.querySelector('#endGame');
     const replay = document.querySelector('#replay');
 
-
-function hideDisplay(){
-    replay.style.visibility = "hidden";
-};
-
-function showDisplay(){
-    replay.style.visibility = "visible";
-};
-
-    document.querySelector('#replay').disabled = true;
-    endGame.textContent = "Game on!";
-    endGame.style.color ="green";
+    document.querySelector('#res').textContent = 'Choose Rock, Paper, or Scissors';
 
 function computerPlay() {
     let play = Math.floor(Math.random() * 3);
@@ -67,27 +47,30 @@ function playRound(playerSel, computerSel) {
     if ((playerChoice === 'rock' && computerChoice === 'scissors')
     || (playerChoice === 'paper' && computerChoice === 'rock')
     || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-        playerScore++;
+        computerWins.textContent = `${playerScore += 1}`;
         document.querySelector("#res").textContent = `You win! ${playerChoice.slice(0, 1).toUpperCase()}${playerChoice.slice(1).toLowerCase()} beats ${computerChoice}.`;
     } else if (playerChoice === computerChoice) {
-        tie++;
-        return `It's a draw!`;
+        document.querySelector("#res").textContent = `It's a draw!`;
     } else if ((computerChoice === 'rock' && playerChoice === 'scissors')
     || (computerChoice === 'paper' && playerChoice === 'rock')
     || (computerChoice === 'scissors' && playerChoice === 'paper')) {
-        computerScore++;
+        playerWins.textContent = `${computerScore += 1}`;
         document.querySelector("#res").textContent = `You lose! ${computerChoice.slice(0, 1).toUpperCase()}${computerChoice.slice(1).toLowerCase()} beats ${playerChoice}.`;
     } else if ((playerChoice !== 'rock', 'paper', 'scissors')) {
         document.querySelector("#res").textContent = `Choose rock, paper, or scissors, you idiot.`;
     }
 };
 
-    document.querySelector('#replay').disabled = true;
-    endGame.textContent = "Game on!";
-    endGame.style.color ="green";
+function hideDisplay(){
+    replay.style.visibility = "hidden";
+};
+  document.querySelector('#replay').disabled = true;
+  endGame.textContent = "Game on!";
+  endGame.style.color ="green";
+
 
 function game(){
-    playRound(playerSelection, computerSelection)
+    playRound(playerSel, computerSel)
 
         if(playerScore >= 5){
             document.querySelector('#res').textContent = `You won! Here's the score: ${playerScore} to ${computerScore}.`
@@ -95,26 +78,37 @@ function game(){
             gameOver();
         }
         else if(computerScore >= 5){
-            document.querySelector('#res').textContent = `Ooops! You Lose. Computer beat you by 
-            ${computerScore} to ${playerScore} point(s).` 
+            document.querySelector('#res').textContent = `You Lose. Here's the score: ${computerScore} to ${playerScore}.` 
             document.querySelector('#res').style.color = 'red'
             gameOver();
-        }
+        }       
+};
 
-        else if(tieGame >= 5){
-            document.querySelector('#res').textContent = `You tied! Here's the score: You got ${playerScore}, 
-            The Computer got ${computerScore} and there were ${tieGame} tie games.`;
-            document.querySelector('#res').style.color = 'orange'
-            gameOver();
-        }
-       
-}
+selRock.addEventListener('click', ()=>{
+    playerSel = "rock"
+    computerSel = computerPlay()
+    game()
+});
+selPaper.addEventListener('click', ()=>{
+    playerSel = "paper"
+    computerSel = computerPlay()
+    game()
+});
+selScissors.addEventListener('click', ()=>{
+    playerSel = "scissors"
+    computerSel = computerPlay()
+    game()
+});
+
+function showDisplay(){
+    replay.style.visibility = "visible";
+};
 
 function gameOver(){
     document.querySelector('#rock').disabled = true;
     document.querySelector('#paper').disabled = true;
     document.querySelector('#scissors').disabled = true;
-    endGame.textContent = "Game Over!";
+    endGame.textContent = "Game Over";
     endGame.style.color = 'red';
     document.querySelector('#replay').disabled = false;
     replay.textContent = "Restart";
@@ -131,12 +125,10 @@ function playAgain(){
     document.querySelector('#scissors').disabled = false;
     endGame.textContent = "Game on!";
     endGame.style.color = "green";
-    document.querySelector('#instructions').textContent = "Select Rock, Paper or Scissors to play";
+    document.querySelector('#res').textContent = "Select Rock, Paper or Scissors to play";
     playerScore = 0; 
     computerScore = 0;
-    tieGame = 0;
     computerWins.textContent = computerScore;
     playerWins.textContent = playerScore;
-    drawGame.textContent = tieGame;
     hideDisplay()
 };
